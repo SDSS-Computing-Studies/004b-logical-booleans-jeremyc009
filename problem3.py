4556f0eefaff728d2345a7269501401552c99443
jeremyc009 <jeremyc009@deltalearns.ca>
2020-09-29 15:06:06 -0700

    Update problem3.py

--- problem3.py
+++ problem3.py
@@ -26,3 +26,34 @@ Enter an integer=>4
 Enter an integer=>2
 2,4,5 do not form a Pythagorean triple
 """
+num1=int(input("Enter the first integer: "))
+num2=int(input("Enter the second integer: "))
+num3=int(input("Enter the third integer: "))
+if num1>num2 and num1>num3 and num2>num3:
+    gNum=num1
+    mNum=num2
+    sNum=num3
+elif num1>num2 and num1>num3 and num3>num2:
+    gNum=num1
+    mNum=num3
+    sNum=num2
+elif num2>num1 and num2>num3 and num1>num3:
+    gNum=num2
+    mNum=num1
+    sNum=num3
+elif num2>num1 and num2>num3 and num3>num1:
+    gNum=num2
+    mNum=num3
+    sNum=num1
+elif num3>num1 and num3>num2 and num1>num2:
+    gNum=num3
+    mNum=num1
+    sNum=num2
+elif num3>num1 and num3>num2 and num2>num1:
+    gNum=num3
+    mNum=num2
+    sNum=num1
+if (mNum**2 + sNum**2)==(gNum**2):
+    print(str(sNum)+ "," + str(mNum)+ ","+ str(gNum) +" form a Pythagorean triple.")
+else: 
+    print(str(sNum)+ "," + str(mNum)+ ","+ str(gNum) +" do not form a Pythagorean triple.")
