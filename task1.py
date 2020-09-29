05d403276c112489bb8dfdbca8b5bbe62d8d9334
jeremyc009 <jeremyc009@deltalearns.ca>
2020-09-29 12:03:43 -0700

    updated

--- task1.py
+++ task1.py
@@ -15,3 +15,8 @@ Outputs:
 That is within normal Earth orbit.
 That is not within normal Earth orbit.
 """
+number=float(input("Enter a number "))
+if number>=0.9759 and number<=1.016:
+    print("That is within normal Earth orbit.")
+else:
+    print("That is not within normal Earth orbit.")
--- task2.py
+++ task2.py
@@ -18,3 +18,14 @@ Example:
 Enter a number: 8
 8 is only a perfect cube.
 """
+number=int(input("Enter a number: "))
+cube=number**(1/3)
+square=number**0.5
+sq2=square%2
+c2=cube%2
+if (sq2==1 or sq2==0) and (c2==1 or c2==0):
+    print(str(number) + " is both a perfect square and a perfect cube.")
+elif sq2!=1 and sq2!=0 and c2==1 or c2==0:
+    print(str(number) + " is only a perfect cube.")
+elif sq2==1 or sq2==0 and c2!=1 and c2!=0:
+    print(str(number) + " is only a perfect square.")
